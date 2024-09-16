1d283ab1526b1c488ff96e691b31007bab313d0d
Toon Verwerft <toonverwerft@gmail.com>
2024-09-16 10:34:18 +0200

    Fix dependencies

--- tests/fixtures/mock-phpcov.php
+++ tests/fixtures/mock-phpcov.php
@@ -2,10 +2,13 @@
 <?php
 
 declare(strict_types=1);
+
 $args = $_SERVER['argv'];
-assert('merge' === $args[1]);
-assert('--clover' === $args[2]);
-assert('clover.xml' === $args[3]);
-assert('php-coverage-dir' === $args[4]);
+$assert = static fn (bool $predicate) => $predicate || throw new RuntimeException('Merging coverage failed');
+
+$assert('merge' === $args[1]);
+$assert('--clover' === $args[2]);
+$assert('clover.xml' === $args[3]);
+$assert('php-coverage-dir' === $args[4]);
 
 exit(0);
